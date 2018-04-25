9d68c44db41a8be42817e7a98259470e1663b9ee
LegnaYet <1023868505@qq.com>
2018-04-25 14:44:36 +0800

    初始化

---
+++ src/main/java/com/legna/utils/exceltils/ExcelTilsApplication.java
@@ -0,0 +1,12 @@
+package com.legna.utils.exceltils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelTilsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcelTilsApplication.class, args);
+	}
+}
